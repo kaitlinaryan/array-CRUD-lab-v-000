3d6541ea6e2b35a40e5e08d2af420bee0cc0d0a1
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-04 16:30:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,19 +16,19 @@ array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-array.pop(element)
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-element = array.shift
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-array[1]
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-array[-1]
+array[element]
 end
 
 def retrieve_last_element_from_array(array)
