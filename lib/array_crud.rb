4df99fe16e593cea627fb50da123458bdf31e955
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-04 16:16:37 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,12 @@ numbers = ["1", "2", "3", "4"]
 end
 
 def add_element_to_end_of_array(array, element)
-numbers << "5"
+numbers << ("5")
 
 end
 
 def add_element_to_start_of_array(array, element)
-numbers.unshift "0"
+numbers.unshift ("0")
 end
 
 def remove_element_from_end_of_array(array)
